dfa06b3e1efee90d7c0fbcefec8b1296f7be7384
coteli <avsarcoteli@gmail.com>
2021-01-23 17:53:14 +0300

    23012021 google sign in added

--- src/screens/giris.js
+++ src/screens/giris.js
@@ -7,8 +7,30 @@ import {
   TouchableOpacity,
   Button,
 } from "react-native";
+import * as Google from "expo-google-app-auth";
 
 function Giris({ navigation }) {
+  async function signInWithGoogleAsync() {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId:
+          "677117166908-f6prgv5tedobv47idrprsu1fqkdj9kac.apps.googleusercontent.com",
+        iosClientId:
+          "677117166908-ejgju85cqd897cdgp4lmjn15vohudjlj.apps.googleusercontent.com",
+        scopes: ["profile", "email"],
+      });
+
+      if (result.type === "success") {
+        console.log("giriş yapıldı!");
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      return { error: true };
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={require("../../assets/giris.png")} />
@@ -21,12 +43,7 @@ function Giris({ navigation }) {
         seçerek mesajlaşmaya başlayabilirsiniz!
       </Text>
       <TouchableOpacity>
-        <Button
-          onPress={() => {
-            navigation.push("Anasayfa");
-          }}
-          title="Google İle Giriş Yap"
-        />
+        <Button onPress={signInWithGoogleAsync} title="Google İle Giriş Yap" />
       </TouchableOpacity>
     </View>
   );
